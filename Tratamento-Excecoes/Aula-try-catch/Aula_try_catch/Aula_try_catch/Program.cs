5308ce371ba256abf0e724d85c93d6b4f0cad6d3
Lucas Cordeiro <lucas_cordeiro2000@outlook.com>
2023-05-09 19:53:14 -0300

    Aula - estrutura try-catch

---
+++ Tratamento-Excecoes/Aula-try-catch/Aula_try_catch/Aula_try_catch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				int n1 = Convert.ToInt32(Console.ReadLine());
+				int n2 = Convert.ToInt32((Console.ReadLine()));
+
+				int result = n1 / n2;
+
+				Console.WriteLine(result);
+			}
+			catch (DivideByZeroException)
+			{
+                Console.WriteLine("Division by zero is not allowed!");
+			}
+			catch (FormatException e)
+			{
+                Console.WriteLine("Format error! " + e.Message);
+            }
+        }
+    }
+}
