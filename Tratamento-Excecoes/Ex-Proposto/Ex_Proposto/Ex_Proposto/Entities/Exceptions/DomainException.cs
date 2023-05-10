982daf0d582c82c7e7c5d3036d4db1860b83229d
Lucas Cordeiro <lucas_cordeiro2000@outlook.com>
2023-05-10 20:44:08 -0300

    Exercício proposto realizado - Tratamento de exceções

---
+++ Tratamento-Excecoes/Ex-Proposto/Ex_Proposto/Ex_Proposto/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace Ex_Proposto.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+
+    }
+}
---
+++ Tratamento-Excecoes/Ex-Proposto/Ex_Proposto/Ex_Proposto/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ex_Proposto.Entities;
+using Ex_Proposto.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace Ex_Proposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial Balance: ");
+                double balance = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter the amount for withdraw: ");
+                double withDraw = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(withDraw);
+
+                Console.WriteLine(account);
+
+            }
+			catch (DomainException e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}
